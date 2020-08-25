686ac2a7513db4941b79dd47987894fc4ece73ad
Sean Stephenson <seanmichaelstephenson@gmail.com>
2020-08-25 16:03:50 -0700

    add more data to stages

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,8 @@ import useGlobalHook from '../utils/useGlobalHook';
 import * as actions from '../actions';
 import initialState from '../data/state.json'
 
+console.log(initialState);
+
 const useGlobal = useGlobalHook(React, initialState, actions);
 
 export default useGlobal;
