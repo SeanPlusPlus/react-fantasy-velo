94e1f6b678c735caeda0d1241a4149c18ba0d3cb
Sean Stephenson <seanmichaelstephenson@gmail.com>
2020-08-25 21:29:54 -0700

    more refactor

--- src/components/Stages.jsx
+++ src/components/Stages.jsx
@@ -1,37 +1,7 @@
 import React from 'react';
-import Honor from './Honor';
 import useGlobal from '../store';
-import trophy from '../images/trophy.png'
-import yellow from '../images/yellow.png'
-import green from '../images/green.png'
-import polkadot from '../images/polkadot.png'
-import white from '../images/white.png'
-import combativity from '../images/combativity.png'
-import teams from '../images/teams.png'
-
-const honors = [
-  {
-    key: 'winner', src: trophy,
-  },
-  {
-    key: 'yellow', src: yellow,
-  },
-  {
-    key: 'green', src: green,
-  },
-  {
-    key: 'polkadot', src: polkadot,
-  },
-  {
-    key: 'white', src: white,
-  },
-  {
-    key: 'combativity', src: combativity,
-  },
-  {
-    key: 'teams', src: teams,
-  },
-];
+import Honor from './Honor';
+import stageHonors from '../utils/stageHonors';
 
 function Stages() {
   const [state] = useGlobal();
@@ -53,7 +23,7 @@ function Stages() {
               {stage.number}. {stage.location}, {stage.distance}, {stage.type}
             </div>
             <ul className="stage-results">
-              {honors.map((h) => (
+              {stageHonors.map((h) => (
                 <Honor key={h.key} winner={stage[h.key]} src={h.src} alt={h.key} />
               ))}
             </ul>
---
+++ src/utils/stageHonors.js
@@ -0,0 +1,33 @@
+import trophy from '../images/trophy.png'
+import yellow from '../images/yellow.png'
+import green from '../images/green.png'
+import polkadot from '../images/polkadot.png'
+import white from '../images/white.png'
+import combativity from '../images/combativity.png'
+import teams from '../images/teams.png'
+
+const stageHonors = [
+  {
+    key: 'winner', src: trophy,
+  },
+  {
+    key: 'yellow', src: yellow,
+  },
+  {
+    key: 'green', src: green,
+  },
+  {
+    key: 'polkadot', src: polkadot,
+  },
+  {
+    key: 'white', src: white,
+  },
+  {
+    key: 'combativity', src: combativity,
+  },
+  {
+    key: 'teams', src: teams,
+  },
+];
+
+export default stageHonors;
