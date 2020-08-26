e9b39175c27ed32dd8856cdef140009e9dfc1181
Sean Stephenson <seanmichaelstephenson@gmail.com>
2020-08-25 21:07:41 -0700

    add icons

--- src/components/Stages.jsx
+++ src/components/Stages.jsx
@@ -1,5 +1,23 @@
 import React from 'react';
 import useGlobal from '../store';
+import trophy from '../images/trophy.png'
+import yellow from '../images/yellow.png'
+import green from '../images/green.png'
+import polkadot from '../images/polkadot.png'
+import white from '../images/white.png'
+import combativity from '../images/combativity.png'
+import teams from '../images/teams.png'
+
+function Honor(props) {
+  return (
+    <li>
+      <img className="jersey" src={props.src} alt={props.alt} />
+      <span className="rider">
+        {props.winner.rider} [{props.winner.team}]
+      </span>
+    </li>
+  )
+}
 
 function Stages() {
   const [state] = useGlobal();
@@ -21,7 +39,13 @@ function Stages() {
               {stage.number}. {stage.location}, {stage.distance}, {stage.type}
             </div>
             <ul className="stage-results">
-              <li>Winner: {stage.winner.rider} [{stage.winner.team}]</li>
+              <Honor winner={stage.winner} src={trophy} alt="trophy" />
+              <Honor winner={stage.yellow} src={yellow} alt="yellow" />
+              <Honor winner={stage.green} src={green} alt="green" />
+              <Honor winner={stage.polkadot} src={polkadot} alt="polkadot" />
+              <Honor winner={stage.white} src={white} alt="white" />
+              <Honor winner={stage.combativity} src={combativity} alt="combativity" />
+              <Honor winner={stage.teams} src={teams} alt="teams" />
             </ul>
           </li>
         ))}
