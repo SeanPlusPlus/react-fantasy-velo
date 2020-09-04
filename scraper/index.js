2a33b9f43a5b8dc620be58022fdf69a3cc40607a
Sean Stephenson <seanmichaelstephenson@gmail.com>
2020-09-04 10:28:15 -0700

    add cheerio

--- scraper/index.js
+++ scraper/index.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const fetch = require('node-fetch')
+const cheerio = require('cheerio');
+
+async function getData(items) {
+  const data = Promise.all(
+    items.map(async (i) => await (await fetch(i)).text())
+  )
+  return data
+}
+
+const handleData = (document) => {
+  document.forEach((document) => {
+    const $ = cheerio.load(document);
+    console.log($);
+  });
+}
+
+const resources = ['rankings']
+const base = 'https://www.letour.fr/en'
+const urls = resources.map((r) => (`${base}/${r}`))
+
+getData(urls).then(handleData)
